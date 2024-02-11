372d20a7caa2b888c4f7992e81194050101ba8e3
alparslanufukoglu <alpaslan.ufukoglu@boun.edu.tr>
2024-02-11 18:59:24 +0300

    Added Match Finding Logic
    -FindMatches method added to GridManager

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,5 +1,5 @@
-using System.Security.Cryptography.X509Certificates;
-using UnityEditor.U2D.Sprites;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GridManager : MonoBehaviour
@@ -7,8 +7,8 @@ public class GridManager : MonoBehaviour
     public int gridWidth;
     public int gridHeight;
     public TileView[,] Tiles;
+    [SerializeField] private List<TileView> _destroyList = new ();
     public static GridManager Instance;
-
     private void Awake()
     {
         Instance = this;
@@ -52,6 +52,111 @@ public class GridManager : MonoBehaviour
     public void SwapArrayPosition(Tile tile1, Tile tile2)
     {
         (Tiles[tile1.posX, tile1.posY], Tiles[tile2.posX,tile2.posY]) = (Tiles[tile2.posX,tile2.posY], Tiles[tile1.posX, tile1.posY]);
+        FindMatches(new List<Tile> {tile1,tile2});
     }
     
+    private void FindMatches(IReadOnlyCollection<Tile> list)
+    {
+        List<int> horizontalCheck = new ();
+        List<int> verticalCheck = new ();
+        foreach (var tile in list.Where(tile => !horizontalCheck.Contains(tile.posY)))
+        {
+            horizontalCheck.Add(tile.posY);
+        }
+        foreach (var tile in list.Where(tile => !verticalCheck.Contains(tile.posX)))
+        {
+            verticalCheck.Add(tile.posX);
+        }
+        foreach (var val in horizontalCheck)
+        {
+            FindHorizontalMatches(val);
+        }
+        foreach (var val in verticalCheck)
+        {
+            FindVerticalMatches(val);
+        }
+    }
+    private void FindHorizontalMatches(int posY)
+    {
+        var horizontalMatches = new List<TileView>();
+        var targetType = TileType.None;
+        for (var i = 0; i < gridWidth; i++)
+        {
+            TileView checkTile = Tiles[i, posY];
+            if (Tiles[i, posY] != null)
+            {
+                if (targetType == TileType.None)
+                {
+                    horizontalMatches.Add(checkTile);
+                    targetType = checkTile.tile.tileType;
+                }
+                else if (checkTile.tile.tileType == targetType)
+                {
+                    if (!horizontalMatches.Contains(checkTile))
+                    {
+                        horizontalMatches.Add(checkTile);
+                    }
+                    if (i == gridWidth - 1)
+                    {
+                        if (horizontalMatches.Count >= 3)
+                        {
+                            _destroyList.AddRange(horizontalMatches);
+                        }
+                    }
+                }
+                else
+                {
+                    if (horizontalMatches.Count >= 3)
+                    {
+                        _destroyList.AddRange(horizontalMatches);
+                    }
+                    horizontalMatches.Clear();
+                    horizontalMatches.Add(checkTile);
+                    targetType = checkTile.tile.tileType;
+                }
+            }
+        }
+    }
+    private void FindVerticalMatches(int posX)
+    {
+        var verticalMatches = new List<TileView>();
+        var targetType = TileType.None;
+        for (int i = 0; i < gridHeight; i++)
+        {
+            TileView checkTile = Tiles[posX, i];
+            if (Tiles[posX, i] != null)
+            {
+                if (targetType == TileType.None)
+                {
+                    verticalMatches.Add(checkTile);
+                    targetType = checkTile.tile.tileType;
+                }
+                else if (checkTile.tile.tileType == targetType)
+                {
+                    if (!verticalMatches.Contains(checkTile))
+                    {
+                        verticalMatches.Add(checkTile);
+                    }
+
+                    if (i == gridHeight - 1)
+                    {
+                        if (verticalMatches.Count >= 3)
+                        {
+                            _destroyList.AddRange(verticalMatches);
+                        }
+                    }
+                }
+                else
+                {
+                    if (verticalMatches.Count >= 3)
+                    {
+                        _destroyList.AddRange(verticalMatches);
+                    }
+                    verticalMatches.Clear();
+                    verticalMatches.Add(checkTile);
+                    targetType = checkTile.tile.tileType;
+                }
+            }
+        }
+    }
 }
