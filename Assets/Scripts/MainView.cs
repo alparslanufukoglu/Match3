535e05ae5a18fb1f90dd7096e6b485d266491738
alparslanufukoglu <alpaslan.ufukoglu@boun.edu.tr>
2024-03-05 15:37:23 +0300

    Added Options Menu
    - Imported UI Assets

--- Assets/Scripts/MainView.cs
+++ Assets/Scripts/MainView.cs
@@ -7,15 +7,14 @@ public class MainView : MonoBehaviour
 {
     [SerializeField] private GameObject mainMenu;
     [SerializeField] private GameObject levelsMenu;
+    [SerializeField] private GameObject optionsMenu;
     [SerializeField] private Image image;
     [SerializeField] private Sprite[] backgroundImages;
-
     private void Start()
     {
         EventManager.Instance.OnMainMenuActivated += ActivateMainMenu;
         EventManager.Instance.OnPlayButtonClicked += ChangeBackgroundImage;
     }
-
     public void LevelsButtonClicked()
     {
         mainMenu.SetActive(false);
@@ -24,14 +23,30 @@ public class MainView : MonoBehaviour
         LevelTableView.Instance.table.ReloadData(0);
         LevelTableView.Instance.table.ScrollToCellAt(0, withMargin: true);
     }
-
-    private void ActivateMainMenu()
+    public void ActivateMainMenu()
     {
         mainMenu.SetActive(true);
     }
-
     private void ChangeBackgroundImage(int index)
     {
         image.sprite = backgroundImages[index];
+    }
+    public void OptionsButtonClicked()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+    public void DeactivateLevelsMenu()
+    {
+        levelsMenu.SetActive(false);
+    }
+    public void DeactivateOptionsMenu()
+    {
+        optionsMenu.SetActive(false);
+    }
+    private void OnDestroy()
+    {
+        EventManager.Instance.OnMainMenuActivated -= ActivateMainMenu;
+        EventManager.Instance.OnPlayButtonClicked -= ChangeBackgroundImage;
     }
 }
